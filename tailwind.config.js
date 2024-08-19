2f48e8399215ffc9a3cc2d2c25cbde1efb64abbf
송이현 <songehyun00@gmail.com>
2024-08-19 17:16:34 +0900

    작업진행

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./*.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
